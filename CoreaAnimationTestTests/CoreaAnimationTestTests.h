e73b23725f710d2d31822b8e9548a02e63671d72
Ippei Sumida <digi.ippey@gmail.com>
2012-11-12 16:55:03 +0900

    Initial Commit

---
+++ CoreaAnimationTestTests/CoreaAnimationTestTests.h
@@ -0,0 +1,13 @@
+//
+//  CoreaAnimationTestTests.h
+//  CoreaAnimationTestTests
+//
+//  Created by Ippei Sumida on 12/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoreaAnimationTestTests : SenTestCase
+
+@end
---
+++ CoreaAnimationTestTests/CoreaAnimationTestTests.m
@@ -0,0 +1,32 @@
+//
+//  CoreaAnimationTestTests.m
+//  CoreaAnimationTestTests
+//
+//  Created by Ippei Sumida on 12/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreaAnimationTestTests.h"
+
+@implementation CoreaAnimationTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoreaAnimationTestTests");
+}
+
+@end
